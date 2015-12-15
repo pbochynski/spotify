dfc4544fa7f124f15d13f621cf4fbd0865fd8d55
Bochynski, Piotr <piotr.bochynski@hybris.de>
2015-12-16 00:09:42 +0100

    Starting point for students

--- statistics.js
+++ statistics.js
@@ -1,9 +1,9 @@
 var app = require('express')();
 var RateLimiter = require('limiter').RateLimiter;
 var limiter = new RateLimiter(5, 1000);
-var CircuitBreaker = require('circuit-breaker-js');
 var request = require('request');
-var breaker = new CircuitBreaker({windowDuration:3000});
+var CircuitBreaker = require('circuit-breaker-js');
+var breaker = new CircuitBreaker({windowDuration: 3000});
 
 function slow(number, callback) {
 	limiter.removeTokens(1, function (err, remainingRequests) {
@@ -27,28 +27,19 @@ function latency(time) {
 
 app.get('/statistics/:number', function (req, res) {
 	var time = process.hrtime();
-	var fallback = function () {
-		var json = {fallback: req.params.number, latency: latency(time)};
-		res.send(json);
-	};
 
-	var command = function (success, failure) {
-		request.get(req.protocol + '://' + req.get('host') + '/slow/' + req.params.number, {timeout:500},
-			function (err, response, body) {
-				if (err) {
-					var errorRes = {error: err, latency: latency(time)};
-					res.status(500).send(errorRes);
-					return failure();
-				}
-				var json = JSON.parse(body);
-				json.latency = latency(time);
-				console.log(json);
-				res.send(json);
-				success();
-			});
-	};
+	request.get(req.protocol + '://' + req.get('host') + '/slow/' + req.params.number, {},
+		function (err, response, body) {
+			if (err) {
+				var errorRes = {error: err, latency: latency(time)};
+				return res.status(500).send(errorRes);
+			}
+			var json = JSON.parse(body);
+			json.latency = latency(time);
+			console.log(json);
+			res.send(json);
+		});
 
-	breaker.run(command, fallback);
 });
 
 exports.statisticsApp = app;
